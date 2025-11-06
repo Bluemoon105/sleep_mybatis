bf999aa9a641a74ada55701d86203557777c9842
Bluemoon105 <118960498+Bluemoon105@users.noreply.github.com>
2025-11-06 19:38:18 +0900

    Add files via upload

---
+++ sleep_mybatis/src/main/java/com/example/sleep/SleepApplication.java
@@ -0,0 +1,11 @@
+package com.example.sleep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepMybatisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SleepMybatisApplication.class, args);
+    }
+}
